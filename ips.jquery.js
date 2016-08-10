cd582c1f5fa59d07eee1844e1c4a693913a6b1d9
xichen <putcncx@gmail.com>
2016-08-10 15:41:09 -0700

    init checkin for pub and sub

--- ips.jquery.js
+++ ips.jquery.js
@@ -1,27 +1,62 @@
 (function($, Deferred) {
     var instances = {};
-    var Event = function (params) {
-        
-    }
-    var Receipt = function (params) {
-        
-    }
+    
     var IPS = function () {
         var events = {};
-        
+
         this.pub = function (subject, data) {
-            var dfd = Deferred();
-            return dfd.promise();
+            var promises = [];
+            var eventRepo = events[subject];
+            var promiseToReturn;
+            if(!eventRepo){
+                var dfd = Deferred();
+                dfd.resolve();
+                promiseToReturn = dfd.promise();
+            }else{
+                eventRepo.before.forEach(function (subscriber) {
+                    var promise = subscriber.callback.apply(subscriber.scope, [data]);
+                    promises.push(promise);
+                })
+            }
+            promiseToReturn = $.when(promises);
+            setTimeout(function (params) {
+                promiseToReturn.then(function (params) {
+                    eventRepo.after.forEach(function (subscriber) {
+                        subscriber.callback.apply(subscriber.scope || window, [data]);
+                    })
+                })
+            }, 0)
+            return promiseToReturn;
         }
         this.sub = function (subject, timing, callback, scope) {
-            var receipt = new Receipt(arguments);
+            var receipt = {
+                subject: subject,
+                timing: timing,
+                callback: callback,
+                scope: scope
+            };
+            if(!events[subject]){
+                events[subject] = {
+                    before: [],
+                    after: []
+                };
+            }
+            var subscriptions = events[subject][timing];
+            subscriptions.push(receipt);
             return receipt;
         }
         this.unsub = function (receipt) {
+            //TODO
             return true;
         }
     }
     $.ips = function (instanceId) {
+        if(!instanceId){
+            if(!defaultInstance){
+                defaultInstance = new IPS();
+            }
+            return defaultInstance;
+        }
         var instance = instances[instaceId];
         if(!instance){
             instances[instaceId] = instance = new IPS();
